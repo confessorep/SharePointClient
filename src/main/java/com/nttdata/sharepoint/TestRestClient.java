cfc3af631c2028e58c671511255aedab2c6bdb02
confessorep <paolo.confessore@nttdata.com>
2015-11-10 15:55:01 +0100

    first commit

--- src/main/java/com/nttdata/sharepoint/TestRestClient.java
+++ src/main/java/com/nttdata/sharepoint/TestRestClient.java
@@ -1,93 +1,186 @@
 package com.nttdata.sharepoint;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
-
+import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
+import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.NTCredentials;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.AuthCache;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.jboss.resteasy.client.ClientExecutor;
-import org.jboss.resteasy.client.ClientRequest;
-import org.jboss.resteasy.client.ClientRequestFactory;
-import org.jboss.resteasy.client.ClientResponse;
-import org.jboss.resteasy.client.jaxrs.ClientHttpEngine;
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
 
 public class TestRestClient {
 
+	private static final String E2E_MODE_DIGEST_URI = "/solutions/e2e/mode/_api/contextinfo";
+	private static final String E2E_MODE_LIST_URI = "/solutions/e2e/mode/_api/web/lists/getbytitle('Documents')/items";
 	
-	private static final String username = "";
-	private static final String password = "";
+	private static final String E2E_MODE_LIST_DOCUMENTS_URI = "/solutions/e2e/mode/_api/web/getfolderbyserverrelativeurl('/All Documents')/files";
+	
+	private static final String APPLICATION_JSON_ODATA_VERBOSE = "application/json;odata=verbose";
+	private static final String HTTPS = "https";
+	private static final String PEERS_HOST_ADDRESS = "areas.it.nttdata-emea.com";
+	private static final String username = "confessorep";
+	private static final String password = "Clin123???";
+	
+	private static final Logger logger =  LoggerFactory.getLogger(TestRestClient.class);
 	
 	
+	static {
+		BasicConfigurator.configure();
+	}
 	
-	private static final String urlDigest = "https://areas.it.nttdata-emea.com/solutions/e2e/mode/_api/contextinfo";
-	
-	
-	public static ClientHttpEngine createEngine() {
+	private static CloseableHttpClient createHttpClient() {
 		CloseableHttpClient httpclient = HttpClientBuilder.create().build();
-
-		HttpHost targetHost = new HttpHost("areas.it.nttdata-emea.com/solutions/e2e/mode", 443, "https");
-		
-		CredentialsProvider credsProvider = new BasicCredentialsProvider();
-		credsProvider.setCredentials(
-		        new AuthScope(targetHost.getHostName(), targetHost.getPort()),
-		        new UsernamePasswordCredentials("confessorep", "Clin123???"));
-
-		// Create AuthCache instance
-		AuthCache authCache = new BasicAuthCache();
-		// Generate BASIC scheme object and add it to the local auth cache
-		BasicScheme basicAuth = new BasicScheme();
-		authCache.put(targetHost, basicAuth);
-
-		// Add AuthCache to the execution context
-		HttpClientContext context = HttpClientContext.create();
-		context.setCredentialsProvider(credsProvider);
-		context.setAuthCache(authCache);
-		
-	    return new ApacheHttpClient4Engine(httpclient);
+		return httpclient;
+	}
+	
+	
+	
+	/*
+	public static ClientHttpEngine createEngine() {
+		return new ApacheHttpClient4Engine(createHttpClient());
 	}
 	
 	public static String callRestEasyRetrieveDigest() {
-		String output = "nothing";
 		try {
 			ClientHttpEngine engine = createEngine();
 			ResteasyClient client = new ResteasyClientBuilder().httpEngine(engine).build();
-			WebTarget target = client.target(urlDigest);
-			Response response = target.request().accept("application/json;odata=verbose").post(Entity.text(""));
+			WebTarget target = client.target(E2E_MODE_DIGEST_URI);
+			Response response = target.request().accept(APPLICATION_JSON_ODATA_VERBOSE).post(Entity.text(""));
 			int status = response.getStatus();
-			System.out.println("Response status: " + status);
-			
-			
+			logger.info("Response status: " + status);
+			MultivaluedMap<String,Object> headers = response.getHeaders();
+			Set<Entry<String,List<Object>>> entrySet = headers.entrySet();
+			logger.info("================ HEADERS ==============");
+			for (Entry<String, List<Object>> entry : entrySet) {
+				logger.info("{}", entry);
+			}
+			logger.info("=======================================");
 		} catch (Exception e) {
 			e.printStackTrace();
 			return e.getMessage();
 		}
 		return null;
 	}
+	*/
+	
+	public static HttpClientContext createContext() {
+		HttpHost targetHost = new HttpHost(PEERS_HOST_ADDRESS, 443, HTTPS);
+		NTCredentials credentials = new NTCredentials(username, password, "", "");
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(new AuthScope(targetHost.getHostName(), targetHost.getPort()), credentials);
+		HttpClientContext context = HttpClientContext.create();
+		context.setCredentialsProvider(credsProvider);
+		return context;
+	}
+	
+	
+	public static void postDigestHttpClient() throws Exception {
+		CloseableHttpClient httpClient = createHttpClient();
+		HttpHost target = new HttpHost(PEERS_HOST_ADDRESS, 443, HTTPS);
+		// Execute an expensive method next reusing the same context (and connection)
+		HttpPost httppost = new HttpPost(E2E_MODE_DIGEST_URI);
+		httppost.addHeader("Accept", APPLICATION_JSON_ODATA_VERBOSE);
+		httppost.setEntity(new StringEntity(""));
+		HttpClientContext context = createContext();
+		CloseableHttpResponse response = httpClient.execute(target, httppost, context);
+		try {
+			Header[] allHeaders = response.getAllHeaders();
+			logger.info("================ HEADERS ==============");
+			for (int i = 0; i < allHeaders.length; i++) {
+				logger.info("{}", allHeaders[i]);
+			}
+			logger.info("=======================================");
+			StatusLine statusLine = response.getStatusLine();
+			logger.info("Response status: {}", statusLine);
+		    HttpEntity entity = response.getEntity();
+		    if(entity != null) {
+		    	String stringResponse = new String(IOUtils.toByteArray(entity.getContent()));
+		    	logger.info("Response: {}", stringResponse);
+		    	String digestForm = parseJsonDigestValue(stringResponse);
+		    	logger.info("Digest form value: {}", digestForm);
+		    }
+		} finally {
+		    response.close();
+		}
+	}
+	
+	
+	public static void getListItems() throws Exception {
+		CloseableHttpClient httpClient = createHttpClient();
+		HttpHost target = new HttpHost(PEERS_HOST_ADDRESS, 443, HTTPS);
+		// Execute an expensive method next reusing the same context (and connection)
+		HttpGet httpget = new HttpGet(E2E_MODE_LIST_DOCUMENTS_URI);
+		httpget.addHeader("Accept", APPLICATION_JSON_ODATA_VERBOSE);
+		HttpClientContext context = createContext();
+		CloseableHttpResponse response = httpClient.execute(target, httpget, context);
+		try {
+			Header[] allHeaders = response.getAllHeaders();
+			logger.info("================ HEADERS ==============");
+			for (int i = 0; i < allHeaders.length; i++) {
+				logger.info("{}", allHeaders[i]);
+			}
+			logger.info("=======================================");
+			StatusLine statusLine = response.getStatusLine();
+			logger.info("Response status: {}", statusLine);
+		    HttpEntity entity = response.getEntity();
+		    if(entity != null) {
+		    	String stringResponse = new String(IOUtils.toByteArray(entity.getContent()));
+		    	logger.info("Response: {}", stringResponse);
+		    	//String digestForm = parseJsonDigestValue(stringResponse);
+		    	//logger.info("Digest form value: {}", digestForm);
+		    }
+		} finally {
+		    response.close();
+		}
+	}
+	
+	
+	
+
+    /**
+     * Retrieves the digest value from the incoming json String
+     * 
+     * @param json
+     * @return
+     */
+    public static String parseJsonDigestValue(String json) {
+        JSONObject jsonObject = (JSONObject) JSONSerializer.toJSON(json);
+        jsonObject = (JSONObject) jsonObject.get("d");
+        jsonObject = (JSONObject) jsonObject.get("GetContextWebInformation");
+        return jsonObject.getString("FormDigestValue");
+
+    }
+    
+    
+    
 	
 	public static void main(String[] args) {
-		callRestEasyRetrieveDigest();
+		try {
+			postDigestHttpClient();
+			getListItems();
+			
+			
+			
+			
+		} catch (Exception e) {
+			logger.error("Error: {}", e.getMessage());
+			logger.error("Stack trace is: ", e);
+		}
 	}
 }
